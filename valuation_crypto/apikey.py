99106297739835892edaf6b55fe602d24229077c
jpegaitaz <jbrivero@protonmail.ch>
2025-03-08 00:37:48 +0100

    Add empty placeholder for apikey.py

---
+++ valuation_crypto/apikey.py
@@ -0,0 +1,18 @@
+
+#OPENAI
+
+openai_key = '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'
+
+#Reddit
+
+client_id='T9w5K2rjl2xw45PsSzVhWg'
+
+client_secret='SOIogJzfQ1a8ELL7BJSFOiFRreA-wg'
+
+user_agent='DS2/1.0 by hereNows'
+
+username='jriverop@gmail.com'
+
+#CoinMarketCap
+
+cmc_api = "8c9e9649-20a3-4a9e-8f48-b93b4f13a60d"
