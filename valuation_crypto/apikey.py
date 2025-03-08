6e5ff1d0476fe9e77aa8abe84c642c52b0db879f
jpegaitaz <jbrivero@protonmail.ch>
2025-03-08 01:40:07 +0100

    fix

--- valuation_crypto/apikey.py
+++ valuation_crypto/apikey.py
@@ -1,18 +1,18 @@
 
 #OPENAI
 
-openai_key = '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'
+openai_key = ''
 
 #Reddit
 
-client_id='T9w5K2rjl2xw45PsSzVhWg'
+client_id=''
 
-client_secret='SOIogJzfQ1a8ELL7BJSFOiFRreA-wg'
+client_secret=''
 
-user_agent='DS2/1.0 by hereNows'
+user_agent=''
 
-username='jriverop@gmail.com'
+username=''
 
 #CoinMarketCap
 
-cmc_api = "8c9e9649-20a3-4a9e-8f48-b93b4f13a60d"
+cmc_api = ""
